21a5ed7a6c36e85e49e337f1184bf007539d3af7
Jacopo <jacopo.rodighiero@icloud.com>
2020-03-04 20:20:15 +0100

    going home

--- scripts/api.js
+++ scripts/api.js
@@ -1,12 +1,47 @@
-let randomElement = [];
 let discoverButton = document.getElementById("discover-button");
-let calendar = document.getElementById('set-bday')
+let calendar = document.getElementById('set-bday');
 
 
-function getDate(){
-    return calendar.value
+//GET DATA FROM CALENDAR
+
+function getDate() {
+    let fullDate = calendar.value.split('-')
+    return fullDate
 }
 
-function lastDay(){
-return getdate.slice[-1]
+let lastNum = ""
+let birth = getDate()
+
+
+birth.forEach(e => {
+    console.log(e[e.length-1])
+    lastNum+=(e[e.length-1])
+})
+
+function getNum(){
+    return lastNum
 }
+
+
+// BUTTON FUNCTIONS
+
+function submitDate(){
+    let calendar = [...document.getElementById('discovery-button').value]
+    return calendar
+}
+
+discoverButton.addEventListener('click', function (event) {
+    console.log('submitted')
+
+    event.submitDate()
+    });
+
+// CALL TO API
+
+function getData(){
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${getNum}`)
+
+}
+getData()
+
+
