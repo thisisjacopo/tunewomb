e7cb9b0fd6ee34e82f439d775a52979b60d743a5
Jacopo <jacopo.rodighiero@icloud.com>
2020-03-04 15:38:04 +0100

    deploy

---
+++ scripts/api.js
@@ -0,0 +1,12 @@
+let randomElement = [];
+let discoverButton = document.getElementById("discover-button");
+let calendar = document.getElementById('set-bday')
+
+
+function getDate(){
+    return calendar.value
+}
+
+function lastDay(){
+return getdate.slice[-1]
+}
